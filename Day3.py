fa2699c990e62e5e7d9e334250161bb14ece6310
dotzo <jaddrum@gmail.com>
2019-12-05 15:46:08 -0600

    day 3, start day 4

--- Day3.py
+++ Day3.py
@@ -9,3 +9,28 @@ def parse(s):
     d = {'R':(1,0), 'L':(-1,0), 'U':(0,1), 'D':(0,-1)}
     return (d[s[0]], int(s[1:]))
 
+def makeWire(path):
+    node = (0,0)
+    nodes = []
+    for i in path:
+        direction, count = parse(i)
+        for _ in range(0,count):
+            node = add(node, direction)
+            nodes.append(node)
+
+    return nodes
+
+def distance(p):
+    return abs(p[0]) + abs(p[1])
+
+def delay(w1, w2, p):
+    return w1.index(p) + w2.index(p) + 2
+
+def intersections(p,q):
+    return list(set(p) & set(q))
+
+wire1 = makeWire(path1)
+wire2 = makeWire(path2)
+
+
+print(min(map(lambda p: delay(wire1, wire2, p), intersections(wire1, wire2))))
